7243c91508c5734a9f07642fb260ae220496079e
poorlarper <jynxzisjunko@gmail.com>
2026-03-28 21:43:26 -0400

    Update discord.js

--- assets/js/discord.js
+++ assets/js/discord.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const userId = "879145354573000714"; // Replace with your hardcoded user ID
+    const userId = "1438288426934734998"; // Replace with your hardcoded user ID
     const apiUrl = `https://discord-lookup-api-alpha.vercel.app/v1/user/${userId}`;
 
     // Elements
